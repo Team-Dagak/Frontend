2626ffc9269724573d56c17d059ea1d8b5aa2bbd
Jaewook Kim <yearsingle@naver.com>
2025-05-16 15:54:10 +0900

    fix: Styling 글래스모피즘, 보더 등

--- src/component/swipeToDelete/deletePopup.jsx
+++ src/component/swipeToDelete/deletePopup.jsx
@@ -11,15 +11,16 @@ const container = css`
   width: 361px;
   height: 226px;
   padding: 18px;
-  background-color: white;
-  border-radius: 12px;
+  background-color: #ffffff10;
+  border-radius: 16px;
+  backdrop-filter: blur(15px);
+  outline-color: #1a1c20;
+  outline-style: solid;
+  outline-width: 1px;
 
   top: 50%;
   left: 50%;
   transform: translate(-50%, -50%);
-  outline-color: #1a1c20;
-  outline-style: solid;
-  outline-width: 1px;
 `;
 
 const giveUp = css`
@@ -27,16 +28,18 @@ const giveUp = css`
     font-weight: bold;
     font-size: large;
     align-items: center;
-    padding-left: 10px;
+    padding-left: 16px;
+
 `;
 
 const explanation = css`
-  display:flex;
+  display: block;
   font-size: small;
-  align-items: center;
-  text-align: center;
-  justify-content: center;
-  padding: 10px;
+  align-content: center;
+  padding: 0 16px;
+  width: 100%;
+  line-height: 1.5rem;
+  word-break: keep-all;
 `;
 
 const choice = css`
@@ -50,7 +53,8 @@ const Retry = styled.button`
   border-radius: 12px;
   background-color: white;
   color: #1a1c20;
-  border: 1px solid #1a1c20;
+  border: none;
+  
 `;
 
 const Retire = styled.button`
@@ -69,7 +73,7 @@ export default function DeletePopup({ onConfirm, onCancel }) {
   return (
     <div css={container}>
       <div css={giveUp}>작은 목표를 정말 삭제할까요?</div>
-      <div css={explanation}> # {taskType}에 설정한 작은 목표 {taskName} 은/는 삭제되며 다시 복구할 수 없어요. </div>
+      <div css={explanation}> <b># {taskType}</b> 에 설정한 작은 목표&nbsp;<b>{taskName}</b> 은/는 삭제되며 다시 복구할 수 없어요. </div>
       <div css={choice}>
         <Retry onClick={onCancel}>다시 생각해볼래요</Retry>
         <Retire onClick={onConfirm}>네, 삭제할래요</Retire>
