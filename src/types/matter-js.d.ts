e77eded8f54bbaa878cbe0dfe11b3a55f2bb412f
Jaewook Kim <yearsingle@naver.com>
2025-11-16 06:37:38 +0900

    fix: build bugs, unused file

--- src/types/matter-js.d.ts
+++ src/types/matter-js.d.ts
@@ -1,15 +1,1 @@
-import "matter-js";
-
-declare module "matter-js" {
-  export interface IRenderDefinition {
-    sprite?: {
-      texture: string;
-      xScale?: number;
-      yScale?: number;
-    };
-  }
-
-  export interface IBodyDefinition {
-    render?: IRenderDefinition;
-  }
-}
+declare module "matter-js";
